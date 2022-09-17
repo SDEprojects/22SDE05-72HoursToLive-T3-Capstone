601a94bc1852eee9ffa506759a058621dd5282cc
Shaquille Mandy <shaqmandy@gmail.com>
2022-09-16 17:16:49 -0700

    incorportate Davids code and maintained gui look

--- src/main/java/view/JPanel_UserInput.java
+++ src/main/java/view/JPanel_UserInput.java
@@ -4,19 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JPanel_UserInput extends JPanel {
+    private String userInput = "";
+
     public JPanel_UserInput() {
         this.setBackground(Color.gray);
         this.setBorder(BorderFactory.createLineBorder(Color.black));
         this.setBounds(0,570,1250,155);
 
+        JTextField userInputTextField = new JTextField(userInput, 82);
+        userInputTextField.setPreferredSize(new Dimension(1250, 150));
+        userInputTextField.setFont(new Font("", Font.PLAIN, 16));
+        userInputTextField.setBackground(new Color(5,8,6));
+        userInputTextField.setForeground(Color.green);
+        userInputTextField.setToolTipText("Users can enter their response here");
 
-        JTextArea userInputTextArea = new JTextArea("go west", 9, 103);
-        userInputTextArea.setBackground(new Color(5,8,6));
-        userInputTextArea.setForeground(Color.green);
-        userInputTextArea.setLineWrap(true);
-        userInputTextArea.setWrapStyleWord(true);
-        userInputTextArea.setToolTipText("Users can enter their response here");
-        JScrollPane scrollPane = new JScrollPane(userInputTextArea);
-        this.add(userInputTextArea);
+        userInputTextField.addActionListener(e -> {
+            userInput = userInputTextField.getText();
+            userInputTextField.setText("");
+        });
+
+        this.add(userInputTextField);
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
     }
 }
